bf4be5e0356f6e26567f62dd836bdeb031a86ce6
DominicDev <s0l0estudi4r@gmail.com>
2026-02-26 13:25:26 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'PANTALEO';
+export const nameSongs = [
+	"L'APARTAMENT", /*1*/
+	"YHUMI", /*2*/
+	"MARTÍ", /*3*/
+	"LA FERA I JO", /*4*/
+	"RITA", /*5*/
+	"DIËRESI", /*6*/
+	"ROMANTICIDI", /*7*/
+	"PI", /*8*/
+	"DIES QUE ENYORO", /*9*/
+	"SOLDATS O GUERRERS", /*10*/
+	"L'EDAT DEL TEMPS", /*11*/
+	"PIETAT AMANDA", /*12*/
+	"CORB", /*13*/
+	"SUBTERFUGIS", /*14*/
+	"LIVE AFLUENTS ", /*15*/
+	"THE STRANGER", /*16*/
+	"A POSTCARD", /*17*/
+	"MIGHTY", /*18*/
+	"INTO THE SUN", /*19*/
+	"THE STRANGER", /*20*/
+]
